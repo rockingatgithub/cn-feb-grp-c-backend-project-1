e434740e50ba55c1ca67dd4f7fa8a02d26100f3d
singhzrepo <contactwithsid@gmail.com>
2022-09-29 20:20:52 +0530

    added passport to express app

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const passport = require('passport')
+const cookieParser = require('cookie-parser')
 const db = require('./config/mongoose')
 const PORT = 8000
 const app = express()
@@ -6,6 +8,13 @@ const app = express()
 
 app.use(express.urlencoded())
 app.use(express.json())
+app.use(cookieParser())
+app.use(require('express-session')({ secret: 'blah', resave: true, saveUninitialized: true }));
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
 app.use('/', require('./routes'))
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const { createUser, getUser } = require('../controllers')
 const User = require('../models/user')
+const passport = require('../config/passport-local')
 const router = express.Router()
 
-router.get('/getData', getUser)
+router.get('/getData', passport.authenticate('local', { failureRedirect: '/login' }) , getUser)
 
 router.post('/createUser', createUser)
+
+router.post('/login', createSession)
 
 module.exports = router
